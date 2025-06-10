bfa2f67f01bc876efdb35596c9679e45f708df95
JorgeFaisal23 <jorgefaisal23@gmail.com>
2025-06-09 23:48:27 -0600

    tercer commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="w-full bg-yellow-400 text-black px-6 py-4 flex justify-between items-center shadow-md">
-      <h1 className="text-2xl font-extrabold uppercase">ExamenApp</h1>
+      <h1 className="text-2xl font-extrabold uppercase">Examen</h1>
       <ul className="flex space-x-6 font-medium">
         <li>
           <Link to="/" className="hover:underline">Home</Link>
